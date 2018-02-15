8e06d7011eb1e3169e33f147cf5f8dbdcf46ebec
HKProductions <hkproductions9@gmail.com>
2018-02-15 01:26:59 -0800

    Attemp to finish assignment 2

---
+++ Assignment 2/Assignment 2/doublyList.cpp
@@ -0,0 +1,165 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct node
+{
+	float data;
+	node *next;
+	node *prev;
+};
+
+class doublyList {
+private:
+	node *head, *tail;
+	int listSize;
+
+public:
+	doublyList();
+	int search(float, string);
+	void insert(float, int);
+	void remove(int);
+	void display();
+};
+
+doublyList::doublyList()
+{
+	head = NULL;
+	listSize = 0;
+}
+
+int doublyList::search(float x, string z)
+{
+	node *temp = head;
+	node *pre = new node();
+	node *cur = new node();
+
+	//Declare local variables
+	int pos = 0;
+
+	if (z == "delete") {
+		//Go through list and see if the float exists
+		if (temp == NULL) {
+			return -1;
+		}
+
+		while (temp != NULL) {
+			if (temp->data == x)
+				return pos;
+
+			else {
+				temp = temp->next;
+				pos++;
+			}
+		}
+	}
+
+	else if (z == "insert") {
+		//Check if the linked list is empty;
+		if (head == NULL)
+			return 0;
+
+		while (temp->next != NULL) {
+			pre->data = temp->data;
+
+			if (temp->data >= x)
+				return pos;
+
+			else {
+				temp = temp->next;
+				pos++;
+			}
+
+			cur->data = temp->data;
+		}
+
+		if ((x >= pre->data && x <= cur->data) || (pre->data == 0 || cur->data == 0))
+			return pos;
+
+		else {
+			pos++;
+			return pos;
+		}
+	}
+	return -1;
+}
+
+void doublyList::insert(float x, int y)
+{
+	node *temp = head;
+	node *Insert = new node();
+	Insert->data = x;
+	Insert->next = NULL;
+	Insert->prev = NULL;
+
+	listSize = listSize++;
+
+	if (y == 0) {
+		Insert->next = head;
+		head = Insert;
+	}
+
+	else if (y < listSize - 1) {
+		for (int i = 0; i < y - 1; i++)
+			temp = temp->next;
+
+		node *store = temp->next;
+		Insert->prev = temp;
+		Insert->next = store;
+		temp->next = Insert;
+		store->prev = Insert;
+	}
+
+	else {
+		for (int i = 0; i < y - 1; i++)
+			temp = temp->next;
+
+		Insert->prev = temp;
+		temp->next = Insert;
+	}
+}
+
+void doublyList::remove(int y)
+{
+	node *Remove = head;
+	node *storeNext = new node();
+	node *storePrev = new node();
+
+	listSize = listSize--;
+
+	if (y == 0) {
+		head = head->next;
+		delete(Remove);
+	}
+
+	else if (y < listSize - 1) {
+		for (int i = 0; i < y; i++)
+			Remove = Remove->next;
+		storeNext = Remove->next;
+		storePrev = Remove->prev;
+
+		storeNext->prev = storePrev;
+		storePrev->next = storeNext;
+		delete(Remove);
+	}
+
+	else {
+		for (int i = 0; i < y; i++)
+			Remove = Remove->next;
+
+		storePrev = Remove->prev;
+		storePrev->next = NULL;
+		delete(Remove);
+	}
+}
+
+void doublyList::display()
+{
+	node *temp = head;
+	while (temp != NULL) {
+		cout << temp->data << endl;
+		temp = temp->next;
+	}
+}
---
+++ Assignment 2/Assignment 2/lineCounter.cpp
@@ -0,0 +1,124 @@
+#include "stdafx.h"
+#include "lineCounter.h"
+
+lineCounter::lineCounter(string fileName)
+{
+	this->fileName = fileName;
+}
+
+void lineCounter::ObjectsAndFunctions()
+{
+	length = 0;
+	string line = "";
+
+	int objectsCount = 0;
+	int functionsCount = 0;
+	input.open(fileName.c_str());
+	while (getline(input, line)) {
+		if (!ignore(line)) {
+			if (line[line.find("class")] == 'c') {
+				status = 1;
+				objectName.push_back(getClassName(line));
+			}
+			if (line[line.find("(")] == '(' && line[line.find(");")] == ')' && status == 1)
+				functionsCount++;
+			if (line[line.find("(")] == '(' && line[line.find("){")] == ')' && status == 1)
+				functionsCount++;
+			if (line[line.find("};")] == '}' && status == 1) {
+				objectsCount++;
+				functionCount.push_back(functionsCount);
+				objectCount.push_back(objectsCount);
+				functionsCount = 0;
+				objectsCount = 0;
+				status = 0;
+			}
+			if (status == 1)
+				objectsCount++;
+
+			length++;
+		}
+	}
+	input.close();
+}
+
+void lineCounter::LOC()
+{
+	string line = "";
+	int objectSize = objectName.size();
+	int index = 0;
+	int functionsCount = 0;
+	int lineCount = 0;
+	int bracket1 = 0;
+	int bracket2 = 0;
+	status = 0;
+	string currentObject = objectName[index];
+	input.open(fileName.c_str());
+	while (getline(input, line)) {
+		if (!ignore(line)) {
+			if (index < objectSize) {
+				currentObject = objectName[index];
+				if (line[line.find("{")] == '{')
+					bracket1++;
+				if (line[line.find("}")] == '}')
+					bracket2++;
+				if (line[line.find("}")] == '}' && status == 1 && bracket1 == bracket2) {
+					lineCount++;
+					functionsCount++;
+					if (functionCount[index] == functionsCount) {
+						int total = objectCount[index] + lineCount;
+						objectCount[index] = total;
+						functionsCount = 0;
+						lineCount = 0;
+						index++;
+						bracket1 = 0;
+						bracket2 = 0;
+					}
+					status = 0;
+				}
+
+				if (line[line.find(currentObject+"::")] == currentObject[0] && line[line.find("){")] == ')' && status == 0)
+					status = 1;
+				if (status == 1)
+					lineCount++;
+			}
+		}
+	}
+	input.close();
+
+}
+
+void lineCounter::print()
+{
+	cout << "Lines of Code" << endl;
+	for (int i = 0; i < objectName.size(); i++) {
+		cout << endl;
+		cout << "Class Name: " << objectName[i] << endl;
+		cout << "Total Amount of Functions: " << functionCount[i] << endl;
+		cout << "Total Number Lines of Code: " << objectCount[i] << endl;
+		cout << endl;
+	}
+	cout << "Total Number Lines of Code in Entire Program: " << length << endl;
+}
+
+bool lineCounter::ignore(string a)
+{
+	cout << a << endl;
+	if (a.length() == 0 || a[a.find("//")] == '/')
+		return true;
+
+	return false;
+}
+
+string lineCounter::getClassName(string name)
+{
+	string result = "";
+	int index = name.find("class");
+	name.replace(index, 5, "");
+	if (name[name.find(":")] == ':') {
+		for (int i = 0; i < name.find(":"); i++) {
+			if (name[i] != ' ')
+				result += name[i];
+		}
+	}
+	return result;
+}
